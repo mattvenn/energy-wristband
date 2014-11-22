39ca2815fccf8f14acb0ab50c0aa22f6fa689ac9
matt venn <matt@mattvenn.net>
2014-11-22 12:30:16 +0000

    fixed random on pi, better validatipn

--- send.py
+++ send.py
@@ -8,43 +8,41 @@ ble_mac = "E4:E2:39:0A:C5:A9"
 ble_mac = "E1:40:D8:62:ED:1A" #other rfduino
 ble_host = 'hci0'
 
+if os.uname()[1] == 'mattsmac':
+    random = '-t random'
+else:
+    random = ''
+
 def handler(signum, frame):
     print 'Signal handler called with signal', signum
     raise IOError("Couldn't open device!")
 
-#read buffer with this
-#/usr/bin/gatttool -b E4:E2:39:0A:C5:A9 -t random --char-read --handle=0x000e
-
 def send_energy(start,end):
     #energy = int(energy * 255)
     SEND = hex(start)[2:].zfill(2) + hex(end)[2:].zfill(2) 
-    if os.uname()[1] == 'mattsmac':
-        random = '-t random'
-    else:
-        random = ''
     cmd = "/usr/bin/gatttool -i " + ble_host + " -b " + ble_mac + random + " --char-write --handle=0x0011 --value=" + SEND
     print(cmd)
     do_send(cmd)
 
 def get_battery():
-    cmd = "/usr/bin/gatttool -i " + ble_host + " -b " + ble_mac + " -t random --char-read --handle=0x000e"
+    cmd = "/usr/bin/gatttool -i " + ble_host + " -b " + ble_mac + random + " --char-read --handle=0x000e"
     data = do_send(cmd)
-    print(data)
     #this check is broken: better to split and check num chunks
-    if 'value/descriptor' in data:
-        #got good value
-        byte2 = data.split()[2]
-        byte1 = data.split()[3]
-        hex_val = byte1 + byte2
-        int_val = int(hex_val,16)
-        print(int_val)
-        vcc = 3.3 #should be 3.3
-        batt_level = int_val / 1023.0 * vcc * 2
-        print("batt = %.2fv" % batt_level)
+    if data:
+        if len(data.split()) == 6:
+            #got good value
+            byte2 = data.split()[2]
+            byte1 = data.split()[3]
+            hex_val = byte1 + byte2
+            int_val = int(hex_val,16)
+            print(int_val)
+            vcc = 3.3 #should be 3.3
+            batt_level = int_val / 1023.0 * vcc * 2
+            print("batt = %.2fv" % batt_level)
+        else:
+            print("problem with data: " + data)
     else:
-        print("problem with data: " + data)
- # second byte then first byte to get int
- #   int("02e1", 16)
+        print("got no data")
 
 def do_send(cmd):
     #Set the signal handler and alarm
