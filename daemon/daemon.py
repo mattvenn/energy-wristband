ae85196f1dd895c2dc45012e18330c43aee8ef53
matt venn <matt@mattvenn.net>
2014-12-18 13:12:42 +0000

    reduced logging

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -22,18 +22,19 @@ data_interval = 60 * 10  # seconds
 
 # set up logging to file - see previous section for more details
 log_format = '%(asctime)s %(name)-10s %(levelname)-8s %(message)s'
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.WARNING,
                     format=log_format,
                     filename='reader.log')
 
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
-console.setLevel(logging.INFO)
+console.setLevel(logging.WARNING)
 formatter = logging.Formatter(log_format)
 console.setFormatter(formatter)
 # add the handler to the root logger
 logger = logging.getLogger('')
 logger.addHandler(console)
+logger.warning("daemon started")
 
 # main loop
 last_data = time.time()
