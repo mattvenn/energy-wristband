5206a9dc481dcdf488357d4d720a1f93ab61c3ca
matt venn <matt@mattvenn.net>
2014-12-19 20:22:43 +0000

    refactored

--- daemon/diff_realtime.py
+++ daemon/diff_realtime.py
@@ -1,4 +1,4 @@
-# !/usr/bin/python
+#!/usr/bin/python
 """
 functions to turn instant energy readings into energy differences from 1 to 4
 """
@@ -32,31 +32,48 @@ def diff(energy, logging):
 
     dt = datetime.datetime.now()
     # logging.info("got %f W at %s" % (energy, dt))
-    last_energy = None
-    current_energy = None
-    if last['time']:
-        diff_time = (dt - last['time']).total_seconds()
-        # if it's been too long between samples, start again
-        if diff_time < max_time:
-            diff_energy = energy - last['energy']
-            logging.info("diff energy = %f W" % diff_energy)
 
-            # only if it's a big enough difference
-            if abs(diff_energy) > sensitivity:
-                # print dt, diff_energy, last_energy, energy
-                last_energy = int(last['energy'] / energy_per_div) + 1
-                current_energy = int(energy / energy_per_div) + 1
+    # current energy as value from 1 to num_divs
+    current_energy = int(energy / energy_per_div) + 1
+    current_energy = limit(current_energy)
 
-                last_energy = limit(last_energy)
-                current_energy = limit(current_energy)
+    # can't do diff unless we have a last time
+    if last['time'] is None:
+        save_state(dt, energy)
+        return(None, current_energy) 
 
+
+    diff_time = (dt - last['time']).total_seconds()
+
+    # if it's been too long between samples, start again
+    if diff_time > max_time:
+        save_state(dt, energy)
+        return(None, current_energy) 
+
+    diff_energy = energy - last['energy']
+    # update last energy history
+    save_state(dt,energy)
+    logging.info("diff energy = %f W" % diff_energy)
+
+    # last energy as value from 1 to num_divs
+    last_energy = int(last['energy'] / energy_per_div) + 1
+    last_energy = limit(last_energy)
+
+    # only if it's a big enough difference
+    if abs(diff_energy) > sensitivity:
+        return (last_energy, current_energy)
+
+    return(None, current_energy)
+
+def save_state(dt, energy):
+    last = {}
     last['time'] = dt
     last['energy'] = energy
     with open("hist.pk", 'w') as fh:
         pickle.dump(last, fh)
 
-    return (last_energy, current_energy)
-
 if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
     energy = float(sys.argv[1])
-    diff(energy)
+    print(diff(energy,logging))
