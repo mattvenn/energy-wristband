bfd8f9692524bdb8aef040d2cb0b59c422299301
matt venn <matt@mattvenn.net>
2014-12-22 20:27:29 +0000

    fixed bug due to over complicated history

--- daemon/diff.py
+++ daemon/diff.py
@@ -8,7 +8,6 @@ import time
 class diff_energy():
     
     max_time = 30  # seconds before we disregard any large changes
-    min_time = 10  # seconds we have to wait till history point is valid
     num_divs = 4  # relate the change as a number from 1 to num_divs
     max_energy = 3000  # watts - this should be adaptive
     energy_per_div = max_energy / num_divs
@@ -16,7 +15,7 @@ class diff_energy():
 
     def __init__(self, logging):
         self.logging = logging
-        self.hist = []
+        self.hist = None
 
     # limits between 1 and num_divs
     def energy_to_div(self,energy):
@@ -31,48 +30,26 @@ class diff_energy():
 
         return div
 
-    def wipe_hist(self):
-        self.hist = []
-
     def add_hist(self,energy):
-        self.hist.append({"t": time.time(), "e": energy})
-    
-    def get_hist(self):
-        now = time.time()
-        #print now
-        #print self.hist
-
-        # trim old history points
-        #self.logging.debug(self.hist)
-        self.hist = [p for p in self.hist 
-                        if p['t'] + diff_energy.max_time > now]
-
-        #print self.hist
-        # find points old enough
-        valid = [p for p in self.hist
-                    if p['t'] + diff_energy.min_time < now]
-
-        #self.logging.debug(valid)
-        if len(valid) == 0:
-            raise ValueError("not enough history")
-    
-        #print("valid = ", valid)
-        # return oldest valid point
-        #print("last point = ", valid[0])
-        return valid[0]
-
+        self.hist = {"t": time.time(), "e": energy}
 
     # returns the last recorded energy, as long as it wasn't too long ago
     # in which case, return the current energy
     def diff(self, energy):
+
+        now = time.time()
+
         # add new energy to our history
+        hist = self.hist
         self.add_hist(energy)
 
-        # get history point
-        try:
-            hist = self.get_hist()
-        except ValueError as e:
-            self.logging.debug(e)
+        # no history
+        if hist is None:
+            return energy
+
+        # too old
+        if hist["t"] + diff_energy.max_time < now:
+            self.logging.debug("history too old")
             return energy
 
         self.logging.debug("got hist t=%d e=%d" % (hist["t"],hist["e"]))
@@ -81,7 +58,7 @@ class diff_energy():
         diff = float(energy - hist["e"]) / (time.time() - hist["t"] )
         diff = abs(diff)
 
-        # if not enough
+        # if not enough of a change
         self.logging.info("energy diff = %f" % diff)
         if diff < diff_energy.sensitivity: 
             return energy
--- daemon/test.py
+++ daemon/test.py
@@ -23,75 +23,54 @@ class Test_Diff(unittest.TestCase):
         self.assertEqual(self.d.diff(1),1)
 
     def test_time_too_long(self):
-        self.d.wipe_hist()
-        past = time.time() - 10000
-        self.d.diff(1,past)
+        now = time.time()
+        self.d.hist = {"t": now - 10000, "e": 100}
         self.assertEqual(self.d.diff(1),1)
 
     def test_large_change_up(self):
         now = time.time()
-        self.d.hist = [
-                        {"t": now - 30, "e": 101},
-                        {"t": now - 20, "e": 102},
-                        {"t": now - 10, "e": 103},
-                        ]
-        self.assertEqual(self.d.diff(1500,now),102)
+        self.d.hist = {"t": now - 20, "e": 100}
+        self.assertEqual(self.d.diff(1500),100)
 
     def test_large_change_down(self):
         now = time.time()
-        self.d.hist = [
-                        {"t": now - 20, "e": 2000},
-                        ]
-        self.assertEqual(self.d.diff(100,now),2000)
+        self.d.hist = {"t": now - 20, "e": 2000}
+        self.assertEqual(self.d.diff(100),2000)
 
     def test_ignore_large_slow_change(self):
-        now = time.time()
-        self.d.hist = [
-                        {"t": now - 60, "e": 100},
-                        {"t": now - 50, "e": 500},
-                        {"t": now - 40, "e": 1000},
-                        {"t": now - 30, "e": 1500},
-                        {"t": now - 20, "e": 2000},
-                        {"t": now - 10, "e": 2500},
-                        ]
-        self.assertEqual(self.d.diff(3000,now),3000)
+        self.assertEqual(self.d.diff(200),200)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(500),500)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(800),800)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(1000),1000)
 
-    def test_history_length(self):
-        now = time.time()
-        # too young is < min_time 10
-        # too old is > max_time 30
-        self.d.hist = [
-                        {"t": now - 40, "e": 100}, #  too old
-                        {"t": now - 30, "e": 101}, #  too old
-                        {"t": now - 20, "e": 102}, #  first valid
-                        {"t": now - 15, "e": 103}, #  valid
-                        {"t": now - 5, "e": 104},  #  too young
-                        {"t": now - 3, "e": 105},  #  too young
-                        ]
-
-        hist = self.d.get_hist()
-        self.assertEqual(hist['e'],102)
-
-        # 2 old ones should be trimmed so = 4
-        self.assertEqual(len(self.d.hist),4)
 
     def test_ignore_small_change(self):
-        now = time.time()
-        self.d.hist = [
-                        {"t": now - 30, "e": 101},
-                        {"t": now - 20, "e": 102},
-                        {"t": now - 10, "e": 103},
-                        ]
-        now = time.time()
-        self.assertEqual(self.d.diff(200,now),200)
-    
+        self.assertEqual(self.d.diff(200),200)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(250),250)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(300),300)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(350),350)
+
     def test_small_change_over_boundary(self):
         boundary = diff_energy.energy_per_div
         now = time.time()
-        self.d.hist = [
-                        {"t": now - 10, "e": boundary - 10},
-                        ]
-        self.assertEqual(self.d.diff(boundary + 10,now),boundary + 10)
+        self.d.hist = {"t": now - 10, "e": boundary - 10}
+        time.sleep(2)
+        self.assertEqual(self.d.diff(boundary + 10),boundary + 10)
+
+    def test_no_repetition(self):
+        self.assertEqual(self.d.diff(200),200)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(250),250)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(3000),250)
+        time.sleep(2)
+        self.assertEqual(self.d.diff(2800),3000)
 
 if __name__ == '__main__':
     unittest.main()
