6ddf38eeb93af115ad5b1f58b098ed136a5f0ba9
matt venn <matt@mattvenn.net>
2015-01-04 23:47:01 +0000

    client for passing on messages

--- daemon/udp_repeater.py
+++ daemon/udp_repeater.py
@@ -6,8 +6,7 @@ import pickle
 
 
 class UDP_get():
-    def __init__(self,logging, ip='192.168.0.9', port=50000):
-        self.ip = ip
+    def __init__(self,logging, port=50000):
         self.logger = logging
         self.port = port
         self.sock = socket.socket(socket.AF_INET,  # Internet
@@ -21,11 +20,32 @@ class UDP_get():
             msg = pickle.loads(data)
             self.logger.info("got [%d,%d,%d] from %s" % (
                 msg["start"], msg["end"], msg["seq"], addr))
+            try:
+                # send start and end
+                s = wristband(logging, args.address,
+                    timeout = args.timeout)
+                s.send(msg["start"], msg["end"])
+
+            except WB_Exception as e:
+                logging.error(e)
 
 if __name__ == '__main__':
+    import argparse
     import logging
-    logging.basicConfig(level=logging.INFO)
-    u = UDP_get(logging)
-    u.listen()
 
+    parser = argparse.ArgumentParser(description="listen to UDP, pass on to wristband")
+    parser.add_argument('--timeout', action='store', type=int, 
+        help="timeout for gatttool", default=10)
+    parser.add_argument('--port', action='store', type=int, 
+        help="port", default=50000)
+    parser.add_argument('--address', help="BLE address of wristband",
+        default = None, required=True)
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    u = UDP_get(logging, port=args.port)
+
+    #start
+    u.listen()
 
