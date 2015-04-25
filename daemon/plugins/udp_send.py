0f052be3ad5781ef18e4cc6677cf9c538a0359c2
matt venn <matt@mattvenn.net>
2015-04-25 08:30:55 +0100

    sorted logging

--- daemon/plugins/udp_send.py
+++ daemon/plugins/udp_send.py
@@ -3,12 +3,13 @@
 import socket
 import time
 import pickle
+import logging
+log = logging.getLogger(__name__)
 
 
 class UDP_send():
-    def __init__(self,logging, ip='192.168.0.255', port=50000):
+    def __init__(self, ip='192.168.0.255', port=50000):
         self.ip = ip
-        self.logger = logging
         self.port = port
         self.sock = socket.socket(socket.AF_INET,  # Internet
                              socket.SOCK_DGRAM)  # UDP
@@ -25,12 +26,11 @@ class UDP_send():
     def send_udp(self, message):
         data = pickle.dumps(message)
         self.sock.sendto(data, (self.ip, self.port))
-        self.logger.info("sent to %s:%d" % (self.ip, self.port))
+        log.info("sent to %s:%d" % (self.ip, self.port))
 
 if __name__ == '__main__':
-    import logging
     logging.basicConfig(level=logging.INFO)
-    u = UDP_send(logging)
+    u = UDP_send()
 
     for seq in range(4):
         u.send(1, 4, time.time())
